a566ac5805b5026726a6507fb417c9f5f697e91d
RajatVerma1309 <rajatverma1309@gmail.com>
2022-02-25 15:35:33 +0530

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Example_1_Day_24.MvcApplication" Language="C#" %>
---
+++ Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example_1_Day_24.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public string EDOJ { get; set; }
+    }
+}
