4bbe7060caafd2e8d4114860fbe336335c8866bc
RajatVerma1309 <rajatverma1309@gmail.com>
2022-02-25 15:45:12 +0530

    Second commit

---
+++ Controllers/EmpsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example_1_Day_24.Controllers
+{
+    public class EmpsController : Controller
+    {
+        // GET: Emps
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
